b6fdfe507dbcfb42985fda5ece22cbff8bf75a44
Johan Jordaan <djjordaan@gmail.com>
2015-07-23 14:02:18 +0200

    Added the closure construct

---
+++ mtgfool/Base/Closure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using mtgfool.Base;
+
+namespace mtgfool.Base
+{
+	public class Closure	
+	{
+		public bool Execute()
+		{
+			return function.Execute (context, parameters);
+		}
+
+		private IFunction<IContext> function;
+		private IContext context;
+		private Dictionary<string,string> parameters;
+		public Closure (IFunction<IContext> function,IContext context,Dictionary<string,string> parameters)
+		{
+			this.function = function;
+			this.context = context;
+			this.parameters = parameters;
+		}
+	}
+}
+
